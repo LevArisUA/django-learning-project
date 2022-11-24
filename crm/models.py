6d7753758a508b80d748b6747cd49eee128e6cfc
vtald <v.taldenko@gmail.com>
2022-11-24 16:36:59 +0200

    шаблоны

--- crm/models.py
+++ crm/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Orders(models.Model):
+    orders_date_time = models.DateTimeField(auto_now=True)
+    orders_name = models.CharField(max_length=200, verbose_name="Имя")
+    orders_phone_num = models.CharField(max_length=15, verbose_name="Телефон")
+    def __str__(self):
+        return self.orders_name
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Orders
+
 
 # Create your views here.
+def first_page(request):
+    object_list = Orders.objects.all()
+    return render(request, './index.html', {'object_list': object_list, 'b': '12345'})
